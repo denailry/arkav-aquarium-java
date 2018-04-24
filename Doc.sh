a4e0995dbd7b9010606ce5768f9b0619d2c2389f
Daniel Ryan <daniel.ryan@students.itb.ac.id>
2018-04-24 23:24:49 +0700

    Add: shell script to generate javadoc

---
+++ Doc.sh
@@ -0,0 +1,15 @@
+ javadoc -d ../doc/javadoc \
+ 	src/Aquarium/Aquarium.java \
+ 	src/Coin/Coin.java \
+ 	src/Entity/Entity.java \
+ 	src/Fish/Fish.java \
+ 	src/Food/Food.java \
+ 	src/Guppy/Guppy.java \
+ 	src/Item/Item.java \
+ 	src/Piranha/Piranha.java \
+ 	src/Snail/Snail.java \
+ 	src/Space/Space.java \
+ 	src/Tick/Tick.java \
+ 	src/LinkedList/LinkedList.java \
+ 	src/Element/Element.java \
+ 	src/Entity/EntityType.java \
