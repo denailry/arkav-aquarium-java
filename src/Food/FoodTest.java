647379e2771d20dfd4c12bca6088133817e42a86
prisilamichelle <prisilamichelle@yahoo.com>
2018-04-25 19:20:41 +0700

    add FoodTest

--- src/Food/FoodTest.java
+++ src/Food/FoodTest.java
@@ -1,6 +1,5 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotEquals;
 
 public class FoodTest {
