f67d99e4460d4405bb8c3da74feff39b7ebaf67a
prisilamichelle <prisilamichelle@yahoo.com>
2018-04-25 13:42:50 +0700

    add PiranhaTest

--- src/Piranha/PiranhaTest.java
+++ src/Piranha/PiranhaTest.java
@@ -6,16 +6,32 @@ import static org.junit.Assert.assertFalse;
 public class PiranhaTest {
 	@Test
 	public void testPiranhaLastFed(){
-	  Piranha piranha = new Piranha(3,4,100,100);
-	  piranha.setLastFed(10);
-	  int lastFed = piranha.getLastFed();
-	  assertEquals("Should return 10", 10, lastFed);
+	  	Piranha piranha = new Piranha(3,4,100,100);
+	  	piranha.setLastFed(10);
+		int lastFed = piranha.getLastFed();
+	  	assertEquals("Should return 10", 10, lastFed);
 	}
 
 	@Test
 	public void testPiranhaIsAbleToConsume(){
-	  Piranha piranha = new Piranha(3,4,100,100);
-	  Guppy guppy = new Guppy(3,4,50,50);
-	  assertTrue("Should be able to consume", piranha.isAbleToConsume(guppy));
+		Aquarium aquarium = new Aquarium(200,200,200,200);	
+	  	Piranha piranha = new Piranha(3,4,100,100);
+	  	Guppy guppy = new Guppy(3,4,50,50);
+	  	aquarium.add(guppy);
+	  	assertTrue("Should be able to consume", piranha.isAbleToConsume(guppy));
+	}
+
+	@Test
+	public void testPiranhaFindNearestGuppy(){
+		LinkedList<Guppy> guppies = new LinkedList<Guppy>();
+		Guppy guppy1 = new Guppy(9,10,50,50);
+		Guppy guppy2 = new Guppy(3,4,50,50);
+		Guppy guppy3 = new Guppy(20,25,50,50);
+		Piranha piranha = new Piranha(4,5,100,100);
+		guppies.add(guppy1);
+		guppies.add(guppy2);		
+		guppies.add(guppy3);
+		Guppy nearestGuppy = piranha.findNearestGuppy(guppies);
+		assertEquals("Should be guppy2", guppy2, nearestGuppy);
 	}
 }
